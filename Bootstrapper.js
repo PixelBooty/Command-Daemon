db3b5eb4d0b7e38b69b54058deb088eaf75360e5
Steven Adams <steven@adaptivelite.com>
2017-11-18 17:20:20 -0700

    Fixed restart-debug not setting the debug config flag.

--- Bootstrapper.js
+++ Bootstrapper.js
@@ -79,6 +79,6 @@ exports.Bootstrapper = class Bootstrapper{
       this.config.fileName = this.options.config;
     }
     this.config = this.config || {};
-    this.config.debug = ( this.service.cliOptions.command === "debug" );
+    this.config.debug = ( this.service.cliOptions.command === "debug" || this.service.cliOptions.command === "restart-debug" );
   }
 }
